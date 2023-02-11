063307305d59b2186fc203c2fd31acb57d253bb5
khadri2021 <khadri.institutions@gmail.com>
2023-02-11 09:16:09 +0530

    update1

--- src/com/khadri/predefined/function/Test.java
+++ src/com/khadri/predefined/function/Test.java
@@ -6,14 +6,19 @@ public class Test {
 
 	public static void main(String[] args) {
 
-		System.out.println("start of the program");
-
-		Predicate<String> predicate = (msg) -> {
-			return msg.isEmpty();
+		Predicate<String> predicate1 = (str) -> {
+			return !str.isEmpty();
 		};
 
-		boolean result = predicate.test("HELLO");
-		System.out.println("is Empty? " + result);
-		System.out.println("End of the program");
+		boolean result1 = predicate1.test("kHADRI");
+		System.out.println(result1);
+
+		Predicate<Integer> predicate2 = (i) -> {
+			return i < 0;
+		};
+
+		boolean result2 = predicate2.test(1);
+		System.out.println(result2);
+
 	}
 }
